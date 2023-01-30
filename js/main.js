42dd830bfb641b335ceb90953bc00f650a319c13
gr413_grkva <grebenuk13@yandex.ru>
2023-01-30 11:31:44 +0700

    3,4 практики

--- js/main.js
+++ js/main.js
@@ -5,6 +5,21 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks.",
         image: "./img/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale:true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
